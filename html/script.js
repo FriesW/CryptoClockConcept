58a944c42aa7db195c5f8f5009dfda2e3af4af92
FriesW <william.james.fries@gmail.com>
2017-10-22 01:46:45 -0500

    ECB AES Test

--- html/script.js
+++ html/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     
     console.log("Hello world");
+    var bytes = aesjs.utils.utf8.toBytes("ABlockIs16Bytes!");
+    console.log(bytes);
+    var key = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3];
+    var aes = new aesjs.AES(key);
     
+    var enc = aes.encrypt(bytes);
+    console.log(enc);
 });
