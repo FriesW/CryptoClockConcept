6438417cc16351ee9370152db29b9fb84d9b3665
FriesW <william.james.fries@gmail.com>
2017-10-22 02:55:07 -0500

    Working

--- html/script.js
+++ html/script.js
@@ -1,5 +1,22 @@
 var BEGIN_TIMESTAMP = 1508656525;
 var KEY = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9, 3];
+var SECONDS = 15;
+
+
+//Weird stuff to get the string slice on the css property to work
+$.fn.delayCss=function(Time,Name,Value){
+var This=this;
+setTimeout(function(){
+This.css(Name,Value);
+},Time);
+return this;
+};
+
+
+function rand(lb, ub)
+{
+    return Math.floor((Math.random() * (ub - lb)) + lb);
+}
 
 function getTime()
 {
@@ -8,7 +25,7 @@ function getTime()
 
 function getStamp()
 {
-    return parseInt((getTime() - BEGIN_TIMESTAMP) / 5);
+    return parseInt((getTime() - BEGIN_TIMESTAMP) / SECONDS);
 }
 
 function encrypt(data)
@@ -30,16 +47,22 @@ function pushToScreen(enc)
         {
             var slice = chunk & 3; //Which is 0b11
             chunk = chunk >> 2;
-            $(".cell:eq(" + cellIndex + ")").css("background-image", "url(imgs/" + slice + ".png)");
+            var selector = ".cell:eq(" + cellIndex + ")";
+            var init_delay = rand(600, 2500);
+            $(selector).delayCss(init_delay, "background-image", "url(imgs/" + slice + ".png)");
+            $(selector)
+                .animate({'opacity':0}, rand(200,3000))
+                .animate({'opacity':1}, rand(200,3000));
             cellIndex ++;
         }
     }
 }
 
 $(document).ready(function(){
+    var pos = getStamp();
     
     console.log("Hello world");
     setInterval(function(){
-        pushToScreen(encrypt(getStamp()));
-    }, 1000);
+        pushToScreen(encrypt(pos++));
+    }, SECONDS*1000);
 });
